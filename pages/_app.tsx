49703e1ea6ee4073dcb77d8d83242ad73d1554ee
starhn87 <starhn87@gmail.com>
2022-03-06 01:11:29 +0900

    Feat: Next + TS + styled-components 초기 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,79 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html {
+        font-size: 62.5%;
+        scroll-behavior: smooth;
+        overflow-x: hidden;
+    }
+    
+    body {
+        font-size: 1.6rem;
+        font-family: 'Inter', 'sans-serif';
+        user-select: none;
+        -webkit-user-select: none; 
+        -moz-user-select: none;   
+        -ms-user-select: none;     
+        -o-user-select: none;
+    }
+    
+    a {
+        text-decoration: none;
+        color:black;
+    }
+    ul {
+        list-style-type: none;
+    }
+    img{
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+    button{
+      border:none;
+      cursor:pointer;
+      
+      &:focus{
+        outline:none;
+      }
+    }
+    input {
+      outline:none;
+      padding: 0 1.5rem;
+      &:focus::placeholder{
+        color:transparent;
+      }
+    }
+  `;
+
+const theme = {
+  colors: {
+    //@ common style
+    mainColor: "#FF6363",
+    pointColor: "#304ffd",
+    lightblue: "#C5E2EE",
+    starColor: "#fd4",
+  },
+};
+
+export type Theme = typeof theme;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
+export default MyApp;
